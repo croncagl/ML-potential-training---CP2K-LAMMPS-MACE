bb0d82b27a8201d719c2ff4c5fc3db711e51f71f
croncagl <roncagliacesare@gmail.com>
2026-03-11 11:48:20 +0100

    Update SLURM settings and environment variables

--- AIMD-CP2K/run_md.sh
+++ AIMD-CP2K/run_md.sh
@@ -3,21 +3,21 @@
 #SBATCH --job-name=MD_CP2K
 #SBATCH --time=3:00:00           
 #SBATCH --nodes=8                                                                        
-#SBATCH --ntasks-per-node=8      # Number of MPI ranks per node
-#SBATCH --cpus-per-task=8        # Number of OMP threads per rank
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=32 
+#SBATCH --cpus-per-task=8 
 #SBATCH --account=your_account                                                                             
 #SBATCH --hint=nomultithread                                                                           
 #SBATCH --exclusive
 #SBATCH --no-requeue
-#SBATCH --uenv=cp2k/2025.1:v1
+#SBATCH --uenv=cp2k/2026.1:v1
 #SBATCH --view=cp2k
  
 # set environment
-export CP2K_DATA_DIR=/path_to_your_cp2k_data
-export CUDA_CACHE_PATH="/dev/shm/$RANDOM"
-export MPICH_GPU_SUPPORT_ENABLED=1
+export CUDA_CACHE_PATH="/dev/shm/$USER/cuda_cache" 
+export MPICH_GPU_SUPPORT_ENABLED=1 
 export MPICH_MALLOC_FALLBACK=1
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$((SLURM_CPUS_PER_TASK - 1))
 ulimit -s unlimited
 
 # change RESTART value appropriately
@@ -32,7 +32,7 @@ else
 fi 
  
 # run
-srun --cpu-bind=cores /path_to_your/mps-wrapper.sh /user-environment/env/cp2k/bin/cp2k.psmp -i md.inp -o md.out
+srun --cpu-bind=socket /path_to_your/mps-wrapper.sh cp2k.psmp -i md.inp -o md.out
 
 # mark first run as complete
 if [ ! -e "first_run.done" ]; then
