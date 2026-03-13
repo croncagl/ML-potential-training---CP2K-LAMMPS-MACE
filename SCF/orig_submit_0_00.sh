4416f39fe3d15d1c12478899f65e6ac042d8fa3b
croncagl <roncagliacesare@gmail.com>
2026-03-13 19:44:08 +0100

    Reduce walltime and update SLURM options in script

--- SCF/orig_submit_0_00.sh
+++ SCF/orig_submit_0_00.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 
 #SBATCH --job-name=scf
-#SBATCH --time=02:00:00           # HH:MM:SS walltime per SCF
+#SBATCH --time=01:00:00           # HH:MM:SS walltime per SCF
 #SBATCH --nodes=1                 # one node per array task
 #SBATCH --ntasks-per-core=1
-#SBATCH --ntasks-per-node=32       # 4 MPI ranks per node
-#SBATCH --cpus-per-task=8         # 4 OMP threads per rank
+#SBATCH --ntasks-per-node=32      # Number of MPI ranks per node
+#SBATCH --cpus-per-task=8        # Number of OMP threads per rank
 #SBATCH --account=your_account    # CHANGE YOUR ACCOUNT HERE
 #SBATCH --hint=nomultithread      # disable hyperthreading
 #SBATCH --exclusive               # reserve the node exclusively
@@ -15,18 +15,16 @@
 #SBATCH --array=0-29              # launch 30 independent tasks
 
 
-#  environment setup 
-
-export CUDA_CACHE_PATH="/dev/shm/$USER/cuda_cache" 
+#  environment setup
+export CUDA_CACHE_PATH="/dev/shm/$USER/cuda_cache"
 export MPICH_GPU_SUPPORT_ENABLED=1
 export MPICH_MALLOC_FALLBACK=1
 export OMP_NUM_THREADS=$((SLURM_CPUS_PER_TASK - 1))
+ulimit -s unlimited
 
 # ADDED - these stop the crashing due to the large number of jobs launched in the array
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close
-
-ulimit -s unlimited
 
 # path to CP2K binary & input base name
 EXE=/user-environment/env/cp2k/bin/cp2k.psmp
@@ -37,4 +35,4 @@ i=$(printf "%04d" "$SLURM_ARRAY_TASK_ID")
 cd $i
 
 # launch exactly one SCF here
-srun --ntasks-per-node=$SLURM_NTASKS_PER_NODE --cpus-per-task=$SLURM_CPUS_PER_TASK --cpu-bind=cores /your_path_to/mps-wrapper.sh ${EXE}  -i ${INP}.inp -o ${INP}.out
+srun --cpu-bind=socket /path_to_your/mps-wrapper.sh /user-environment/env/cp2k/bin/cp2k.psmp  -i ${INP}.inp -o ${INP}.out
