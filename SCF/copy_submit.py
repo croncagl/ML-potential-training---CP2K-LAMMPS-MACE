eccd59a06134db76049a0a5ccbcb433642e78b3d
croncagl <roncagliacesare@gmail.com>
2026-03-13 19:45:59 +0100

    Update SLURM account in copy_submit.py

--- SCF/copy_submit.py
+++ SCF/copy_submit.py
@@ -16,7 +16,7 @@ with open("submit_all.sh",'w') as f:
     f.write("#SBATCH --job-name=submit_all\n")
     f.write("#SBATCH --time=00:02:00\n")
     f.write("#SBATCH --nodes=1\n")
-    f.write("#SBATCH --account=lp86\n")
+    f.write("#SBATCH --account=your_account\n")     #change here
     f.write("#SBATCH --partition=normal\n")
     f.write("cd sp_conf\n")
     for _folder in range(n_folders):
